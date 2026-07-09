1d707b5416d1f675171cf8c01f5cbe4b6840d8c3
Mingtao <mingtao674@gmail.com>
2026-07-09 15:54:17 +0200

    Delete 第1周第1次第1部分 directory

--- 第1周第1次第1部分/专业化project：第16周/project2/src/view.py
+++
@@ -1,9 +0,0 @@
-def view_watchlist(watchlist):
-    """View all companies in the watchlist."""
-    print("\nYour Watchlist:")
-    if not watchlist:
-        print("Watchlist is empty.")
-    else:
-        for idx, company in enumerate(watchlist, start=1):
-            print(f"{idx}. {company}")
-    print()
