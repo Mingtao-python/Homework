a1e3118543f12414e81fa1ff37442e8c58178376
Mingtao <mingtao674@gmail.com>
2026-07-23 14:37:36 +0200

    Delete 21week directory

--- 21week/project/prompt_filter.py
+++
@@ -1,9 +0,0 @@
-def filter_prompt(p):
-    if len(p) > 300:
-        return False, "Input too long"
-    bad = ["ignore", "override", "bypass"]
-    if any(w in p.lower() for w in bad):
-        return False, "Unsafe prompt detected"
-    if len(p) < 5:
-        return False, "Input too short"
-    return True, p
